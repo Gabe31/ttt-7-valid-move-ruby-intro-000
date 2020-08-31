5d80add114c977a52d6ead8aa43a58d45f642eb7
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-31 05:51:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
 if  index.to_i.between?(0, 8) && !position_taken?(board, index)
     return true
-else
+elsif !index.to_i.between?(0, 8) && position_taken?(board, index)
   return false
 end 
 end
