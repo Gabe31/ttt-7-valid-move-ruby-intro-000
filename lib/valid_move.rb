4761969a3850899a75a910329d568b3fcc07a0ef
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-31 06:20:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,9 @@ end
 
 
 def position_taken?(board, index)
-  if board[index] == "X" or "O"
+  if board[index] == "X" 
+    return true
+  elsif board[index] == "O"  
     return true
   elsif board[index] == " "
     return false
